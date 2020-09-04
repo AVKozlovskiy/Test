8fca158c1fd2afd17b9736041692be7ec074a2f0
AVKozlovskiy <kyzinka@ya.ru>
2020-09-04 23:04:57 +0300

    Double add

---
+++ src/AVKozlovskiy/Main.java
@@ -0,0 +1,22 @@
+package AVKozlovskiy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+            double a = Double.MAX_VALUE + 1;
+            double b = Double.MAX_VALUE;
+            double c = Double.MIN_VALUE;
+            double m = Double.MAX_VALUE + Integer.MAX_VALUE;
+            double x = Double.MAX_VALUE * 2;
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(a == b);
+            System.out.println(a == m);
+            System.out.println(x);
+            System.out.println(c + 1);
+        }
+    }
+}
